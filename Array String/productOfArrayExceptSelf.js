3112841dc5d2d53d65985773ce969a568c0c1c92
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-02-05 07:47:42 +0600

    Added Product of Array except self function

---
+++ Array String/productOfArrayExceptSelf.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    if(!n) return 0;
+    
+    const answer = new Array(n).fill(1);
+
+    let leftProduct = 1;
+    for(let i = 1; i < n; i++){
+        leftProduct *= nums[i - 1];
+        answer[i] *= leftProduct;
+    }
+
+    let rightProduct = 1;
+    for(let i = n -2; i >= 0; i--){
+        rightProduct *= nums[i + 1];
+        answer[i] *= rightProduct;
+    }
+
+    return answer
+};
