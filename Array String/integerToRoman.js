85adea1e49689a06140220d9ab92fde53e8ecf0a
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-02-08 09:31:59 +0600

    Implemented integer to roman find function

---
+++ Array String/integerToRoman.js
@@ -0,0 +1,15 @@
+// /**
+//  * @param {number} num
+//  * @return {string}
+//  */
+const intToRoman = (num) => {
+    //create the list..
+    const M = ["", "M", "MM", "MMM"];
+    const C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+   
+    const result = M[Math.floor(num / 1000)] + C[Math.floor((num % 1000) / 100)] + X[Math.floor((num % 100) / 10)] + I[num % 10];
+    return result;
+};
