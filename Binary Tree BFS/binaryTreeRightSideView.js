e6d11720d05e293f52a8b408416cfba24dc4d84e
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-03-12 09:37:54 +0600

    added binary tree right side view

---
+++ Binary Tree BFS/binaryTreeRightSideView.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(!root) return [];
+   const q = [root];
+   const result = [];
+   while(q.length){
+       const size = q.length;
+       result.push(q[size-1].val);
+       for(let i=0; i<size; i++){
+           const node = q.shift();
+           node.left && q.push(node.left);
+           node.right && q.push(node.right);
+       }
+   }
+   return result;
+};
