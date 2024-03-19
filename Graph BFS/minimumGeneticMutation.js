6b657ada000079461994f2c15dccdb2aa821da9e
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-03-19 09:49:13 +0600

    Added minimum genetic mutation function

---
+++ Graph BFS/minimumGeneticMutation.js
@@ -0,0 +1,40 @@
+/**
+ * @param {string} startGene
+ * @param {string} endGene
+ * @param {string[]} bank
+ * @return {number}
+ */
+
+const minMutation = (start, end, bank) => {
+    const choices = ['A', 'C', 'G', 'T'];
+    const queue = [start];
+    const seen = new Set([start]);
+
+    let steps = 0;
+
+    while (queue.length !== 0) {
+        const nodesInQueue = queue.length;
+
+        for (let j = 0; j < nodesInQueue; j++) {
+            const node = queue.shift();
+
+            if (node === end)
+                return steps;
+
+            for (const choice of choices) {
+                for (let i = 0; i < node.length; i++) {
+                    const neighbor = node.substring(0, i) + choice + node.substring(i + 1);
+
+                    if (!seen.has(neighbor) && bank.includes(neighbor)) {
+                        queue.push(neighbor);
+                        seen.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        steps++;
+    }
+
+    return -1;
+}
