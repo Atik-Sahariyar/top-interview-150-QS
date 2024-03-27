9da4bd9e95cfc9022264a318d065d1051f527e36
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-03-27 10:16:30 +0600

    Added combination sum

---
+++ Backtreacking/combinationSum.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+const combinationSum =  (candidates, target) => {
+    const result = [];
+    const path = [];
+    candidates.sort((a, b) => a - b);
+    backtrack(candidates, 0, target, path, result);
+    return result;
+};
+
+const backtrack = (candidates, start, target, path, result) => {
+    if (target === 0) {
+        result.push([...path]);
+        return;
+    }
+    if (target < 0) {
+        return;
+    }
+    for (let i = start; i < candidates.length; i++) {
+        path.push(candidates[i]);
+        backtrack(candidates, i, target - candidates[i], path, result);
+        path.pop();
+    }
+}
