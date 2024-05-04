0067973d4ee2769c1bac365bf385a7df02df77f4
Atik Sahariyar <atiksahariyar88@gmail.com>
2024-05-04 22:42:04 +0600

    update reverse bits function

---
+++ Bit Manipulation/reverseBits.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+
+// remember the binary must always be of length 32 ;);
+var reverseBits = function(n) {
+    const reversedBin = n.toString(2).split('').reverse().join('');
+    const result =  reversedBin.padEnd(32,'0'); 
+    return parseInt(result, 2);
+};
+
+// updated
